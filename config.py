2ce117388d5a43ec595aca63e12bd130a2c260e1
1chosenone <joeyrabelpaquette@hotmail.com>
2026-03-16 22:23:30 -0400

    pydantic-settings use as a config

--- config.py
+++ config.py
@@ -1,19 +1,34 @@
 import logging
 import os
 from dotenv import load_dotenv
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 # Load variables from .env file
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise ValueError("DATABASE_URL is not set in environment variables")
 
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+class Settings(BaseSettings):
+
+    # Database config
+    database_url: str
+
+    # Logging config
+    log_level: str = "INFO"
+
+    # LLM config
+    anthropic_api_key: SecretStr
+    model: str = "cclaude-haiku-4-5-20251001"
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+# Create a single instance to be used across the app
+settings = Settings()
 
 
 def setup_logging():
     logging.basicConfig(
-        level=LOG_LEVEL,
+        level=settings.log_level,
         format="%(asctime)s | %(levelname)s | %(name)s | %(message)s",
     )
