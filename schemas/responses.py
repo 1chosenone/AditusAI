95eaa64d7e129780a9a8e5fea8624cbb06ef712a
1chosenone <joeyrabelpaquette@hotmail.com>
2026-03-17 22:23:02 -0400

    CandidateResponse schema added

--- schemas/responses.py
+++ schemas/responses.py
@@ -13,3 +13,18 @@ class APIResponse(BaseModel):
 
     status: str
     message: str
+
+
+class CandidateResponse(BaseModel):
+    candidate_id: int
+    first_name: str
+    last_name: str
+    email: str | None
+    city: str | None
+    region: str | None
+    country: str | None
+    bio: str | None
+    github_url: str | None
+    linkedin_url: str | None
+
+    model_config = {"from_attributes": True}
